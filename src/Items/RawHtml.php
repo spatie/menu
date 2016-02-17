ef9763de044163541c52463aa6fdf247fc71d3ea
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-02-17 15:28:14 +0100

    setActive

---
+++ src/Items/RawHtml.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Spatie\Navigation\Items;
+
+use Spatie\Navigation\Item;
+use Spatie\Navigation\Traits\HtmlElement;
+
+class RawHtml implements Item
+{
+    use HtmlElement;
+
+    /** @var string */
+    protected $html;
+
+    /** @var bool */
+    protected $active;
+
+    public function __construct(string $html)
+    {
+        $this->html = $html;
+        $this->active = false;
+    }
+
+    public function html() : string
+    {
+        return $this->html;
+    }
+
+    public function isActive() : bool
+    {
+        return $this->active;
+    }
+
+    public function setActive() : Item
+    {
+        $this->active = true;
+
+        return $this;
+    }
+
+    public function setInactive() : Item
+    {
+        $this->active = false;
+
+        return $this;
+    }
+
+    public function render() : string
+    {
+        return $this->renderHtml(
+            'li',
+            $this->html,
+            $this->isActive() ? ['active'] : []
+        );
+    }
+}
--- src/Menu.php
+++ src/Menu.php
@@ -2,6 +2,8 @@
 
 namespace Spatie\Navigation;
 
+use Spatie\Navigation\Items\Link;
+use Spatie\Navigation\Items\RawHtml;
 use Spatie\Navigation\Traits\Collection;
 use Spatie\Navigation\Traits\HtmlElement;
 use function Spatie\Navigation\callable_parameter_types;
@@ -20,6 +22,16 @@ class Menu
         return new static(...$items);
     }
 
+    public function addLink(string $text, string $url) : Menu
+    {
+        return $this->addItem(new Link($text, $url));
+    }
+
+    public function addHtml(string $html) : Menu
+    {
+        return $this->addItem(new RawHtml($html));
+    }
+
     public function manipulate(callable $callable) : Menu
     {
         $type = get_callable_parameter_types($callable)[0] ?? null;
@@ -31,6 +43,24 @@ class Menu
             }
 
             $callable($item);
+        }
+
+        return $this;
+    }
+
+    public function setActive(callable $callable) : Menu
+    {
+        $type = get_callable_parameter_types($callable)[0] ?? null;
+
+        foreach($this->items as $item) {
+
+            if ($type && ! $item instanceof $type) {
+                continue;
+            }
+
+            if ($callable($item)) {
+                $item->setActive();
+            }
         }
 
         return $this;
