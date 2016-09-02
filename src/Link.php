fac9369b7d1bfdf67533b3ed2086206e51f2de17
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-09-02 16:06:38 +0200

    Created HasUrl trait

--- src/Link.php
+++ src/Link.php
@@ -4,12 +4,13 @@ namespace Spatie\Menu;
 
 use Spatie\HtmlElement\HtmlElement;
 use Spatie\Menu\Traits\Activatable as ActivatableTrait;
+use Spatie\Menu\Traits\HasUrl as HasUrlTrait;
 use Spatie\Menu\Traits\HtmlAttributes;
 use Spatie\Menu\Traits\ParentAttributes;
 
 class Link implements Item, Activatable, HasHtmlAttributes, HasParentAttributes, HasUrl
 {
-    use ActivatableTrait, HtmlAttributes, ParentAttributes;
+    use ActivatableTrait, HasUrlTrait, HtmlAttributes, ParentAttributes;
 
     /** @var string */
     protected $text;
@@ -51,58 +52,6 @@ class Link implements Item, Activatable, HasHtmlAttributes, HasParentAttributes,
     public function getText(): string
     {
         return $this->text;
-    }
-
-    /**
-     * @return string
-     */
-    public function getUrl(): string
-    {
-        if (empty($this->prefixes)) {
-            return $this->url;
-        }
-
-        return implode('', $this->prefixes).'/'.ltrim($this->url, '/');
-    }
-
-    /**
-     * Return a segment of the link's URL. This function works for both absolute
-     * and relative URL's. The index is a 1-index based number. Trailing and
-     * double slashes are ignored.
-     *
-     * Example: (new Link('Open Source', 'https://spatie.be/opensource'))->segment(1)
-     *      => 'opensource'
-     *
-     * @param int $index
-     *
-     * @return string|null
-     */
-    public function segment(int $index)
-    {
-        $path = parse_url($this->url)['path'] ?? '';
-
-        $segments = array_values(
-            array_filter(
-                explode('/', $path),
-                function ($value) {
-                    return $value !== '';
-                }
-            )
-        );
-
-        return $segments[$index - 1] ?? null;
-    }
-
-    /**
-     * @param string $prefix
-     *
-     * @return $this
-     */
-    public function prefix(string $prefix)
-    {
-        $this->prefixes[] = $prefix;
-
-        return $this;
     }
 
     /**
---
+++ src/Traits/HasUrl.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Spatie\Menu\Traits;
+
+/**
+ * Expects a `$url` and a `$prefixes` property on the class.
+ *
+ * @property string $url
+ * @property array $prefixes
+ */
+trait HasUrl
+{
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        if (empty($this->prefixes)) {
+            return $this->url;
+        }
+
+        return implode('', $this->prefixes) . '/' . ltrim($this->url, '/');
+    }
+
+    /**
+     * Return a segment of the link's URL. This function works for both absolute
+     * and relative URL's. The index is a 1-index based number. Trailing and
+     * double slashes are ignored.
+     *
+     * Example: (new Link('Open Source', 'https://spatie.be/opensource'))->segment(1)
+     *      => 'opensource'
+     *
+     * @param int $index
+     *
+     * @return string|null
+     */
+    public function segment(int $index)
+    {
+        $path = parse_url($this->url)['path'] ?? '';
+
+        $segments = array_values(
+            array_filter(
+                explode('/', $path),
+                function ($value) {
+                    return $value !== '';
+                }
+            )
+        );
+
+        return $segments[$index - 1] ?? null;
+    }
+
+    /**
+     * @param string $prefix
+     *
+     * @return $this
+     */
+    public function prefix(string $prefix)
+    {
+        $this->prefixes[] = $prefix;
+
+        return $this;
+    }
+}
