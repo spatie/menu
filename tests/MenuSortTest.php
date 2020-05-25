e70fdf26cb64a3165c8a7974cdf121f2f5940cc9
Jean Rumeau <rumeau@gmail.com>
2020-05-25 19:24:48 -0400

    Update MenuSortTest.php
    Fix tests

--- tests/MenuSortTest.php
+++ tests/MenuSortTest.php
@@ -8,11 +8,24 @@ use Spatie\Menu\Menu;
 class MenuSortTest extends MenuTestCase
 {
     /** @test */
+    public function element_has_priority()
+    {
+        $this->menu = Menu::new()
+            ->add(Link::to('/contact', 'Contact')->setPriority(10));
+
+        $items = $this->menu->getIterator();
+        $first = $items[0];
+
+        $this->assertTrue(method_exists($first, 'getPriority'));
+        $this->assertEquals(10, $first->getPriority());
+    }
+
+    /** @test */
     public function uses_default_order()
     {
         $this->menu = Menu::new()
-          ->add(Link::to('/', 'Home'))
-          ->add(Link::to('/contact', 'Contact'));
+            ->add(Link::to('/', 'Home'))
+            ->add(Link::to('/contact', 'Contact'));
 
         $this->assertRenders('
             <ul>
@@ -21,26 +34,13 @@ class MenuSortTest extends MenuTestCase
             </ul>
         ');
     }
-    
-    /** @test */
-    public function element_has_priority()
-    {
-        $this->menu = Menu::new()
-            ->add(Link::to('/contact', 'Contact'))->setPriority(10);
-            
-        $items = $this->menu->getIterator();
-        $first = $items[0];
-        
-        $this->assertTrue(method_exists($first, 'getPriority'));
-        $this->assertEquals(10, $first->getPriority());
-    }
-    
+
     /** @test */
     public function sort_based_on_priority()
     {
         $this->menu = Menu::new()
-            ->add(Link::to('/contact', 'Contact'))->setPriority(10)
-            ->add(Link::to('/', 'Home'))->setPriority(0);
+            ->add(Link::to('/contact', 'Contact')->setPriority(10))
+            ->add(Link::to('/', 'Home')->setPriority(0));
 
         $this->assertRenders('
             <ul>
@@ -49,15 +49,15 @@ class MenuSortTest extends MenuTestCase
             </ul>
         ');
     }
-    
+
     /** @test */
     public function modify_sorting_process()
     {
         $this->menu = Menu::new()
-            ->add(Link::to('/contact', 'Contact'))->setPriority(10)
-            ->add(Link::to('/', 'Home'))->setPriority(100)
+            ->add(Link::to('/contact', 'Contact')->setPriority(10))
+            ->add(Link::to('/', 'Home')->setPriority(100))
             ->setSortCallback(function ($a, $b) {
-                return $a->getPriority > $b->getPriority ? -1 : 1;
+                return $a->getPriority() > $b->getPriority() ? -1 : 1;
             });
 
         $this->assertRenders('
@@ -67,16 +67,16 @@ class MenuSortTest extends MenuTestCase
             </ul>
         ');
     }
-    
+
     /** @test */
     public function try_another_sorting_approach()
     {
         $this->menu = Menu::new()
-            ->add(Link::to('/contact', 'Contact'))->setPriority(10)
-            ->add(Link::to('/', 'Home'))->setPriority(100)
+            ->add(Link::to('/contact', 'Contact')->setPriority(10))
+            ->add(Link::to('/', 'Home')->setPriority(100))
             ->setSortCallback(function ($a, $b) {
                 return strcmp($a->text(), $b->text());
-               
+
             });
 
         $this->assertRenders('
