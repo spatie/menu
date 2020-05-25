3044379982ce9ca5e7493bb0e181843f67b4f9fe
Jean Rumeau <rumeau@gmail.com>
2020-05-25 17:51:55 -0400

    Create MenuSortTest.php
    Add tests

---
+++ tests/MenuSortTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Spatie\Menu\Test;
+
+use Spatie\Menu\Link;
+use Spatie\Menu\Menu;
+
+class MenuSortTest extends MenuTestCase
+{
+    /** @test */
+    public function uses_default_order()
+    {
+        $this->menu = Menu::new()
+          ->add(Link::url('/', 'Home'))
+          ->add(Link::url('/contact', 'Contact'));
+
+        $this->assertRenders('
+            <ul>
+                <li><a href="/">Home</a>
+                <li><a href="/contact">Contact</a>
+            </ul>
+        ');
+    }
+    
+    /** @test */
+    public function sort_based_on_priority()
+    {
+        $this->menu = Menu::new()
+            ->add(Link::url('/contact', 'Contact'))->setPriority(10)
+            ->add(Link::url('/', 'Home'))->setPriority(0);
+
+        $this->assertRenders('
+            <ul>
+                <li><a href="/">Home</a>
+                <li><a href="/contact">Contact</a>
+            </ul>
+        ');
+    }
+    
+    /** @test */
+    public function modify_sorting_process()
+    {
+        $this->menu = Menu::new()
+            ->add(Link::url('/contact', 'Contact'))->setPriority(10)
+            ->add(Link::url('/', 'Home'))->setPriority(100)
+            ->setSortCallback(function ($a, $b) {
+                return $a->getPriority > $b->getPriority ? -1 : 1;
+            });
+
+        $this->assertRenders('
+            <ul>
+                <li><a href="/">Home</a>
+                <li><a href="/contact">Contact</a>
+            </ul>
+        ');
+    }
+    
+    /** @test */
+    public function try_another_sorting_approach()
+    {
+        $this->menu = Menu::new()
+            ->add(Link::url('/contact', 'Contact'))->setPriority(10)
+            ->add(Link::url('/', 'Home'))->setPriority(100)
+            ->setSortCallback(function ($a, $b) {
+                return strcmp($a->text(), $b->text());
+               
+            });
+
+        $this->assertRenders('
+            <ul>
+                <li><a href="/contact">Contact</a>
+                <li><a href="/">Home</a>
+            </ul>
+        ');
+    }
+}
